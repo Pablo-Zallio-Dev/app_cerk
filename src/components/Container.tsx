ec4a19fcf56209ebb073d895ff472e0dd1cbc241
Pablo Zallio <pablozalliodev@gmail.com>
2026-07-27 12:21:32 +0200

    style: correcting paddings

--- src/components/Container.tsx
+++ src/components/Container.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Container = ( {children}: {children: React.ReactNode} ) => {
   return (
-    <section className=" flex flex-col justify-center h-dvh bg-bg-secondary ">
+    <section className=" flex flex-col justify-center py-5 bg-bg-secondary ">
       {children}
     </section>
   )
